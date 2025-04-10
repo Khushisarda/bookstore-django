42c1b3805ff23dbdac819dcaf03b63bf4e076ce5
KhushiSarda <khushisarda2005@gmail.com>
2025-04-10 11:26:00 +0530

    Enhanced UI, added book images, base layout, and migrations

--- store/models.py
+++ store/models.py
@@ -1,10 +1,12 @@
+
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='book_images/', blank=True)
 
     def __str__(self):
         return self.title
-
